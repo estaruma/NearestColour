6e4a5a8ba7c554ed8811e71db6f0ee9f019c2459
estaruma <esther.arumainayagam@mettle.co.uk>
2024-09-25 08:14:08 +0100

    kata - Part 1: nearest color

--- src/main/java/org/example/NearestColour.java
+++ src/main/java/org/example/NearestColour.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class NearestColour {
+    private static String[] predefinedColours = {"F00", "0F0", "00F"};
+
+    public static String NearestColour(String inputHex) {
+        Colour inputColour = new Colour(inputHex);
+        double minDistance = Double.MAX_VALUE;
+        String nearestColour = null;
+
+
+        for (String colourHex : predefinedColours) {
+            Colour predefinedColour = new Colour(colourHex);
+            double distance = inputColour.distanceTo(predefinedColour);
+
+            if(distance <minDistance) {
+                minDistance = distance;
+                nearestColour = colourHex;
+            }
+        }
+
+        return nearestColour;
+    }
+
+
+
+
 }
